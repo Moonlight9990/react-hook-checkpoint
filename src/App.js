1cfdc723352d29e1c686bb8712023c941867ea5c
Moonlight9990 <lioswag6@gmail.com>
2023-08-29 10:39:28 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import MovieList from './components/MovieList';
+import Filter from './components/Filter';
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([
+    // Initial movie data
+    {
+      title: 'Movie 1',
+      description: 'Description 1',
+      posterURL: 'poster-url-1',
+      rating: 7.5,
+    },
+    // Add more movies
+  ]);
+
+  const [titleFilter, setTitleFilter] = useState('');
+  const [ratingFilter, setRatingFilter] = useState('');
+
+  const filteredMovies = movies.filter(movie => {
+    return (
+      movie.title.toLowerCase().includes(titleFilter.toLowerCase()) &&
+      movie.rating >= parseFloat(ratingFilter)
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie App</h1>
+      <Filter
+        titleFilter={titleFilter}
+        ratingFilter={ratingFilter}
+        setTitleFilter={setTitleFilter}
+        setRatingFilter={setRatingFilter}
+      />
+      <MovieList movies={filteredMovies} />
     </div>
   );
-}
+};
 
 export default App;
